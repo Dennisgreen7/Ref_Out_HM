b77900f9d6f96a28b7d096a93671a7aa5af61163
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-07-20 20:05:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ref_Out_HM
+{
+    class Program
+    {
+        public static void addnum(ref int num)
+        {
+            num++;
+        }
+        public static void addval(out int num)
+        {
+            Console.WriteLine("Enter num");
+            num = int.Parse(Console.ReadLine());
+        }
+        public static string ValidateValues(string name_,string last_name,string phone_num,string tz,int age, out string erormessege)
+        {
+            erormessege = "";
+            if(name_ == "" || last_name == "")
+            {
+                erormessege = "name or last name are empty";
+                return erormessege;
+            }
+            if(phone_num[0] != '0')
+            {
+                erormessege = " phone number must start with 0";
+                return erormessege;
+            }
+            if(tz.Length != 9)
+            {
+                erormessege = " tz must be 9 digits";
+                return erormessege;
+            }
+            if(age < 1 || age > 120)
+            {
+                erormessege = "age out of rage 1-120";
+
+                return erormessege;
+            }
+            return "True";
+        }
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            //addnum(ref a);
+            //addnum(ref b);
+            //Console.WriteLine("a = "+a+", b = "+b);
+            int c, d;
+            //addval(out c);
+            //addval(out d);
+            //Console.WriteLine("c = " + c + ", d = " + d);
+            string erormessege;
+            string a1 = ValidateValues("d", "greenberg", "0526551089", "315976233", 25,out erormessege);
+            Console.WriteLine(a1);
+            Console.ReadLine();
+        }
+    }
+}
